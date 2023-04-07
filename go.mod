1c373374f4cb105eb8c8419c9ce0beed506680a1
tombracho <artyomsonyx@gmail.com>
2023-04-07 22:15:33 +0300

    Add templates for rendering html, use chi for route

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/tombracho/lenslocked
 
 go 1.18
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.8
+)
--- main.go
+++ main.go
@@ -2,87 +2,56 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
 )
+
+func executeTemplate(w http.ResponseWriter, tplPath string) {
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parthing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
+	}
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	method := r.Method
-	fmt.Println("homeHandler executed with method", method, r.URL.Path)
-	fmt.Fprint(w, "<h1>Hello, World!</h1>")
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	method := r.Method
-	fmt.Println("loginHandler executed with method", method, r.URL.Path)
-	fmt.Fprint(w, "<h1>LOGIN</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	method := r.Method
-	fmt.Println("contactHandler executed with method", method, r.URL.Path)
-	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a href="mailto:artyomsonyx@gmail.com">`)
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
-
-func galleryHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	method := r.Method
-	fmt.Println("galleryHandler executed with method", method, r.URL.Path)
-	fmt.Fprint(w, `<h1>Gallery</h1>`)
-}
-
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Status code 404: not fount")
-	http.Error(w, "Page not found", http.StatusNotFound)
-}
-
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-
-	switch path {
-	case "/":
-		homeHandler(w, r)
-	case "/login":
-		loginHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/gallery":
-		galleryHandler(w, r)
-	default:
-		notFoundHandler(w, r)
-	}
-}
-
-type Router struct{}
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h2>FAQ</h2>")
-}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-
-	switch path {
-	case "/":
-		homeHandler(w, r)
-	case "/login":
-		loginHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	case "/gallery":
-		galleryHandler(w, r)
-	default:
-		notFoundHandler(w, r)
-	}
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Error 404. Page not found")
+		http.Error(w, "Pahe not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
-	//http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", r)
 }
